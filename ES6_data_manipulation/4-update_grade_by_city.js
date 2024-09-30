77e46b65b97214ef71d968afab7a6b409e71ccd1
EyaKadri <eyakaad@gmail.com>
2024-09-30 17:23:43 +0200

    4-update_grade_by_city.js

---
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,12 @@
+export default function updateStudentGradeByCity(students, city, newGrades) {
+  return students.filter((student) => student.location === city)
+    .map((student) => {
+      const gradeInfo = newGrades.find((grade) => grade.studentId === student.id);
+      return {
+        id: student.id,
+        firstName: student.firstName,
+        location: student.location,
+        grade: gradeInfo ? gradeInfo.grade : 'N/A',
+      };
+    });
+}
