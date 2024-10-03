2796f5efcba7f3af75dc55179068341a22b4dbef
EyaKadri <eyakaad@gmail.com>
2024-10-03 18:43:51 +0200

    1-stdin.js

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,10 +1,11 @@
 console.log('Welcome to Holberton School, what is your name?');
 
 process.stdin.on('data', (INPUT) => {
-  const name = INPUT.toString().trim(); // Get the name and trim whitespace
-  console.log(`Your name is: ${name}`); // Display the user's name
+  const name = INPUT.toString().trim();
+  console.log(`Your name is: ${name}`);
+  process.stdin.end();
 });
 
 process.stdin.on('end', () => {
-  console.log('This important software is now closing'); // Closing message
+  console.log('This important software is now closing');
 });
