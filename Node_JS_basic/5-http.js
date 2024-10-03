072065d5b6f3fee0d6347c5f94685774c2505c22
EyaKadri <eyakaad@gmail.com>
2024-10-03 20:59:11 +0200

    5-http.js

---
+++ Node_JS_basic/5-http.js
@@ -0,0 +1,33 @@
+const http = require('http');
+const fs = require('fs');
+
+const app = http.createServer((req, res) => {
+  if (req.url === '/') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello Holberton School!\n');
+  } else if (req.url === '/students') {
+    const dbName = 'path/to/your/database.csv';
+    fs.readFile(dbName, 'utf8', (err, data) => {
+      if (err) {
+        res.writeHead(500, { 'Content-Type': 'text/plain' });
+        res.end('Error reading the database\n');
+        return;
+      }
+      const students = data
+        .split('\n')
+        .filter((line) => line.trim() !== '')
+        .map((line) => line.trim());
+      res.writeHead(200, { 'Content-Type': 'text/plain' });
+      res.end(`This is the list of our students\n${students.join('\n')}\n`);
+    });
+  } else {
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.end('Not Found\n');
+  }
+});
+
+app.listen(1245, () => {
+  console.log('Server listening on port 1245');
+});
+
+module.exports = app;
