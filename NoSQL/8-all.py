64f5b290aa68417dbaa02af566e741f4456ce5ea
ibrahim <ibrahimkammoun@outlook.com>
2024-11-22 20:32:32 +0100

    8-all.py

---
+++ NoSQL/8-all.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+""" MongoDB operations with python using pymongo """
+
+
+def list_all(mongo_collection):
+    """ list all documents
+      """
+    documents = mongo_collection.find()
+
+    if documents.count() == 0:
+        return []
+
+    return documents
