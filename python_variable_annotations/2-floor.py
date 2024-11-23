214282ccb4de3bb878d65e11a5042b4d29d6c8a0
ibrahim <ibrahimkammoun@outlook.com>
2024-11-23 20:40:11 +0100

    2-floor.py

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""module for basic annotations - floor"""
+
+
+def floor(n: float) -> int:
+    """returns the floor of the float
+    Args:
+        n (float): The float to floor
+    Returns:
+        int: The floored number
+    """
+    return int(n)
