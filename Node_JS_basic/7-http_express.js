55cfd57ff66d171a552a375b0cb7e29f1f78a48d
ibrahim <ibrahimkammoun@outlook.com>
2024-11-23 19:11:34 +0100

    7-http_express.js

--- Node_JS_basic/7-http_express.js
+++ Node_JS_basic/7-http_express.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const countStudents = require('./3-read_file_async');
+const fs = require('fs').promises;
 
 const app = express();
 const port = 1245;
@@ -10,10 +10,31 @@ app.get('/', (req, res) => {
 
 app.get('/students', async (req, res) => {
   try {
-    const data = await countStudents(process.argv[2]);
-    res.send(['This is the list of our students', ...data].join('\n'));
+    const data = await fs.readFile(process.argv[2], 'utf8');
+    const lines = data.trim().split('\n').slice(1);
+    const studentsByField = {};
+    
+    lines.forEach(line => {
+      const [firstName, , , field] = line.split(',');
+      if (!studentsByField[field]) {
+        studentsByField[field] = [];
+      }
+      studentsByField[field].push(firstName);
+    });
+
+    const totalStudents = Object.values(studentsByField)
+      .reduce((sum, students) => sum + students.length, 0);
+
+    let response = ['This is the list of our students'];
+    response.push(`Number of students: ${totalStudents}`);
+    
+    Object.entries(studentsByField).forEach(([field, students]) => {
+      response.push(`Number of students in ${field}: ${students.length}. List: ${students.join(', ')}`);
+    });
+
+    res.status(200).send(response.join('\n'));
   } catch (error) {
-    res.send(`This is the list of our students\n${error.message}`);
+    res.status(200).send('This is the list of our students\nCannot load the database');
   }
 });
 
