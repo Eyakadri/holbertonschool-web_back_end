422409e4e21f3cea06817bc24f8132516f0a470b
ibrahim <ibrahimkammoun@outlook.com>
2024-11-23 18:59:09 +0100

    5-http.js

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -1,6 +1,32 @@
 const http = require('http');
 const fs = require('fs').promises;
 
+const processStudentData = (data) => {
+  const lines = data.trim().split('\n').slice(1);
+  const studentsByField = {};
+  lines.forEach((line) => {
+    const [firstName, , , field] = line.split(',');
+    if (!studentsByField[field]) {
+      studentsByField[field] = [];
+    }
+    studentsByField[field].push(firstName);
+  });
+  return studentsByField;
+};
+
+const formatResponse = (studentsByField) => {
+  const response = ['This is the list of our students'];
+  const totalStudents = Object.values(studentsByField)
+    .reduce((sum, students) => sum + students.length, 0);
+  response.push(`Number of students: ${totalStudents}`);
+  Object.entries(studentsByField).forEach(([field, students]) => {
+    response.push(
+      `Number of students in ${field}: ${students.length}. List: ${students.join(', ')}`,
+    );
+  });
+
+  return response.join('\n');
+};
 const app = http.createServer(async (req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
 
@@ -9,26 +35,8 @@ const app = http.createServer(async (req, res) => {
   } else if (req.url === '/students') {
     try {
       const data = await fs.readFile(process.argv[2], 'utf8');
-      const lines = data.trim().split('\n');
-      const students = {};
-      const fields = lines[0].split(',');
-
-      for (const line of lines.slice(1)) {
-        const values = line.split(',');
-        const field = values[values.length - 1];
-        if (!students[field]) students[field] = [];
-        students[field].push(values[0]);
-      }
-
-      let response = ['This is the list of our students'];
-      const total = Object.values(students).reduce((sum, group) => sum + group.length, 0);
-      response.push(`Number of students: ${total}`);
-      
-      for (const [field, names] of Object.entries(students)) {
-        response.push(`Number of students in ${field}: ${names.length}. List: ${names.join(', ')}`);
-      }
-      
-      res.end(response.join('\n'));
+      const studentsByField = processStudentData(data);
+      res.end(formatResponse(studentsByField));
     } catch (error) {
       res.end('This is the list of our students\nCannot load the database');
     }
