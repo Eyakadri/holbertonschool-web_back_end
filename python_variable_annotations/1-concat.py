195d7b71aac8efff1e06d75d0cb390f14a16de56
ibrahim <ibrahimkammoun@outlook.com>
2024-11-23 20:37:03 +0100

    1-concat.py

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""Module for basic annotations - concat"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """returns the concatenated string
+    Args:
+        str1 (str): First string
+        str2 (str): Second string
+    Returns:
+        str: The concatenated strings
+    """
+    return str1 + str2
