1d165b4efcbac513579341be55a1182f59dce6e8
ibrahim <ibrahimkammoun@outlook.com>
2024-11-24 18:50:43 +0100

    0-async_generator.py

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -10,3 +10,10 @@ async def async_generator() -> List[float]:
     for _ in range(10):
         await asyncio.sleep(1)
         yield uniform(0, 10)
+
+async def main():
+    async for number in async_generator():
+        print(number)
+
+if __name__ == "__main__":
+    asyncio.run(main())
