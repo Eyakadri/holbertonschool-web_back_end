999e87449c94c396ef0973c5183da558c75ce1af
ibrahim <ibrahimkammoun@outlook.com>
2024-11-24 20:14:06 +0100

    3-hypermedia_del_pagination.py

--- pagination/3-hypermedia_del_pagination.py
+++ pagination/3-hypermedia_del_pagination.py
@@ -10,8 +10,7 @@ from typing import Dict
 
 
 class Server:
-    """server class to paginate a database of popular baby names
-    """
+    """Server class to paginate a database of popular baby names"""
     DATA_FILE = "Popular_Baby_Names.csv"
 
     def __init__(self):
@@ -19,19 +18,16 @@ class Server:
         self.__indexed_dataset = None
 
     def dataset(self) -> List[List]:
-        """cached dataset
-        """
+        """Cached dataset"""
         if self.__dataset is None:
             with open(self.DATA_FILE) as f:
                 reader = csv.reader(f)
                 dataset = [row for row in reader]
             self.__dataset = dataset[1:]
-
         return self.__dataset
 
     def indexed_dataset(self) -> Dict[int, List]:
-        """dataset indexed by sorting position, starting at 0
-        """
+        """Dataset indexed by sorting position, starting at 0"""
         if self.__indexed_dataset is None:
             dataset = self.dataset()
             truncated_dataset = dataset[:1000]
@@ -40,24 +36,25 @@ class Server:
             }
         return self.__indexed_dataset
 
+    def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
+        """Return a dictionary with pagination information based on index"""
+        assert index is None or 0 <= index < len(self.indexed_dataset())
 
-def get_hyper_index(self, index: int = None, page_size: int = 10) -> dict:
-    """return a dictionary with pagination information based on index"""
-    dataset = self.dataset()
-    data_length = len(dataset)
+        if index is None:
+            index = 0
 
-    if index is None:
-        index = 0
+        next_index = index + page_size
+        data = []
+        i = index
 
-    assert 0 <= index <= data_length, "Index out of range"
+        while len(data) < page_size and i < len(self.indexed_dataset()):
+            if i in self.indexed_dataset():
+                data.append(self.indexed_dataset()[i])
+            i += 1
 
-    data = dataset[index:index + page_size]
-
-    next_index = index + page_size
-
-    return {
-        'index': index,
-        'next_index': next_index,
-        'page_size': page_size,
-        'data': data
-    }
+        return {
+            'index': index,
+            'next_index': next_index,
+            'page_size': len(data),
+            'data': data
+        }
