0e207f169f7fbaecbfae8e08e3a68ebfaae32774
ibrahim <ibrahimkammoun@outlook.com>
2024-11-24 18:44:18 +0100

    0-async_generator.py

---
+++ python_async_comprehension/0-async_generator.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""returns a list of random floats"""
+import asyncio
+from typing import List
+from random import uniform
+
+
+async def async_generator() -> List[float]:
+    """returns a list of random floats"""
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield uniform(0, 10)
